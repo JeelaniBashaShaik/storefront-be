7005f0b2961bdd7d1d47dd952d33e5fe36754891
Jeelani <jeelanshaik07@gmail.com>
2021-05-26 21:40:07 +0530

    be setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://storeFrontAdmin:storeFrontPassword@develop.5toek.mongodb.net/develop?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('connected with mongodb');
+  client.close();
+});
+
+app.listen(3000, function() {
+    console.log('listening on 3000');
+})
+
+app.get('/', function (req, res) {
+    res.send({success: true})
+  })
+
+  app.get('/test', function (req, res) {
+    res.send({testKey: 'test value'});
+  })
---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('./../controllers/user.controller');
+
+/* router.post('/login', loginController.login);
+router.post('/register', loginController.register);
+router.post('/isAuthorized', loginController.isAuthorized); */
+
+module.exports = router;
