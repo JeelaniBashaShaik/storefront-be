97de84249c89e3f4c107f4a70619a8241832adea
Jeelani <jeelanshaik07@gmail.com>
2021-05-27 18:23:45 +0530

    addes cors

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://storeFrontAdmin:storeFrontPassword@develop.5toek.mongodb.net/develop?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const db = client.db("develop");
   // perform actions on the collection object
   console.log('connected with mongodb');
-  client.close();
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    next();
+  });
+
+    const usersCollection = db.collection('users');
+
+    app.post('/user', (req, res) => {
+        console.log(req.body, 'request body');
+        usersCollection.insertOne({ timestamp: new Date(), ...req.body }, ((err, result) => {
+            res.send('ok');
+           }));
+      })
 });
 
 app.listen(process.env.PORT || 5000, function() {
-    console.log('listening on 3000');
+    console.log(process.env.PORT, 'listening');
 })
 
 app.get('/', function (req, res) {
@@ -22,3 +36,5 @@ app.get('/', function (req, res) {
   app.get('/test', function (req, res) {
     res.send({testKey: 'test value'});
   })
+
+ 
